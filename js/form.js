ec78d2be31c7d4bf9a3dfe729d2365201039a2d8
Burak Y <buriii@hotmail.de>
2023-11-13 19:32:10 +0100

    added textarea add

--- js/form.js
+++ js/form.js
@@ -12,20 +12,46 @@
 //   article.append(button);
 // });
 
-const form = document.querySelector(".form");
-const buttonsbt = document.querySelector(".btn-submit");
+// const link = document.querySelector("a");
+// link.addEventListener("click", (event) => {
+//     if (confirm("are you sure you want to leave the page")) {
+//         console.log("leaving");
+//     } else {
+//         console.log("staying");
+//         event.preventDefault();
+//     }
+// });
+
+//const buttonsbt = document.querySelector(".btn-submit");
+
+const form = document.querySelector('[data-js="form"]');
 const wrapper = document.querySelector(".wrapper");
 
-// Event listener for the button within the form
-buttonsbt.addEventListener("click", (event) => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  // Create new elements to be added within the wrapper
-  const article = document.createElement("article");
-  const button = document.createElement("button");
-  button.textContent = "New Button";
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
 
-  // Append the new elements to the wrapper
-  wrapper.appendChild(article);
-  article.appendChild(button);
+  // Create HTML content with the form data
+  const htmlContent = `
+
+  <div class="flex-container">
+      <div class="quiz-item">
+        <h2 class="question">${data["question-text"]}</h2>
+        <button class="btn-quiz">Show answer</button>
+        <p class="show-answer">${data.answer}</p>
+        </p>
+        <div class="tags">
+          <!-- <span class="label success">#HTML</span> -->
+          <span class="label info">${data.tags}</span>
+          <!-- <span class="label warning">#JS</span> -->
+        </div>
+        <img class="bookmark-quiz-img" id= 'img-change' src="img/bookmark.png" alt="" onclick="changeImage()"/>
+      </div>
+    <div>
+  `;
+
+  // Insert the HTML content after the wrapper
+  wrapper.insertAdjacentHTML("afterend", htmlContent);
 });
